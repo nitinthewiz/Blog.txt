d0b4b8dba77e923d86008770e7d3535d5dee61f1
Nitin Khanna <mail@nitinkhanna.com>
2013-03-05 16:33:32 -0700

    Created a working copy of Create
    This new create page requires a password, called a Key here, to post to the blog.
    The password is stored in the Apache vHost config for the site as -
    SetEnv EMITKEY "your-pass"

--- create.php
+++ create.php
@@ -2,6 +2,9 @@
 Copyright (c) 2012, Matt Zanchelli
 All rights reserved.
 
+Edited by Nitin Khanna
+Mar 5 2013
+
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
 	* Redistributions of source code must retain the above copyright
@@ -25,8 +28,11 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 -->
 <?php
-	if ( isset($_POST["filename"]) && isset($_POST["date"]) && isset($_POST["content"]) ) {	// Only works if all three fields are filled out!
-		$dir = "posts";
+	require_once("config.php");
+	error_reporting(E_ALL);
+	
+	if ( isset($_POST["filename"]) && isset($_POST["date"]) && isset($_POST["content"]) && isset($_POST["passkey"]) ) {	// Only works if all four fields are filled out!
+		// $dir = "/var/www/emit.nitinkhanna.com/posts";
 		
 		// $filename = preg_replace("/[^a-zA-Z0-9\._-]/", "", $_POST["filename"]);	// Eh, I'll fix that later
 		$filename = $_POST["filename"];	//	(Temporary) Works with spaces
@@ -41,82 +47,57 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 		$full = $dir . "/" . $filename;	// dir/filename.txt
 		$date = $_POST["date"];
 		$content = $_POST["content"];
-		if ( file_exists( $full ) ) {	//	Overwrites existing file
-			$existing = fopen( $full, w );
-			fwrite ( $existing, $date . "\n" . substr($filename, 0, -4) . "\n" . $content );
-			fclose($existing);
-		} else {
-			$new = fopen( $full, w );	//	Or creates a new file
-			fwrite( $new,  $date . "\n" . substr($filename, 0, -4) . "\n" . $content );
-			fclose($new);
+		// getenv('EMITKEY') 
+		if ($_SERVER['EMITKEY'] == $_POST["passkey"]){
+			if ( file_exists( $full ) ) {	//	Doesn't Overwrite existing file, creates another by that name + 1 (whihc is the wrong implementation)
+				$existing = fopen( $full."1", 'w+' );
+				if (isset($_POST["url"])){
+					fwrite ( $existing, $date . "\n" . "<a href=\"" . $_POST["url"] . "\">" . substr($filename, 0, -4) . "</a>" . "\n" . $content );
+				}
+				else {
+					fwrite ( $existing, $date . "\n" . substr($filename, 0, -4) . "\n" . $content );
+				}
+				fclose($existing);
+				echo "No error "; // . $_SERVER['EMITKEY'] . " is same as " . $_POST["passkey"] . "\n";
+				echo "The following got written - " .  $existing, $date . "\n" . "<a href=\"" . $_POST["url"] . "\">" . substr($filename, 0, -4) . "</a>" . "\n" . $content;
+				
+			} else {
+				$new = fopen( $full, 'w+' );	//	Or creates a new file
+				if (isset($_POST["url"])){
+					fwrite ( $new, $date . "\n" . "<a href=\"" . $_POST["url"] . "\">" . substr($filename, 0, -4) . "</a>" . "\n" . $content );
+				}
+				else {
+					fwrite ( $new, $date . "\n" . substr($filename, 0, -4) . "\n" . $content );
+				}
+				fclose($new);
+				echo "No error "; // . $_SERVER['EMITKEY'] . " is same as " . $_POST["passkey"] . "\n";
+				echo "The following got written - " .  $new, $date . "\n" . "<a href=\"" . $_POST["url"] . "\">" . substr($filename, 0, -4) . "</a>" . "\n" . $content;
+			}
 		}
+		else{
+			echo "error in password"; //" . $_SERVER['EMITKEY'] . " is not same as " . $_POST["passkey"] . "\n";
+		}	
 	}
 ?>
 <!doctype html>
 <html>
-<head>
-	<title>Edit</title>
-	<script type="text/javascript">
-		var bool = true;
-		function edit(one, two, three, a, b, c, submit) {
-			if (bool) {
-				bool = false;
-				document.getElementById(submit).style.display = "";
-				
-				//var elements = document.getElementsByClassName("original");
-				//for (var i=0; i<elements.length; i++) {
-				//	elements[i].style.display = "none";
-				//}
-				document.getElementById(one).style.display = "none";
-				document.getElementById(two).style.display = "none";
-				document.getElementById(three).style.display = "none";
-				
-				document.getElementById(a).style.display = "";
-				document.getElementById(a).value = document.getElementById(one).innerHTML;
-				
-				document.getElementById(b).style.display = "";
-				document.getElementById(b).value = document.getElementById(two).innerHTML;
-				
-				document.getElementById(c).style.display = "";
-				document.getElementById(c).value = document.getElementById(three).innerHTML;
-			}
-			else {
-				bool = true;
-				document.getElementById(submit).style.display = "none";
-				
-				//var editables = document.getElementsByClassName("editable");
-				//for (var i=0; i<editables.length; i++) {
-				//	editables[i].style.display = "";
-				//	editables[i].innerHTML = document.getElementById("a").value;
-				//}
-				
-				document.getElementById(one).style.display = "";
-				document.getElementById(one).innerHTML = document.getElementById(a).value;
-				
-				document.getElementById(two).style.display = "";
-				document.getElementById(two).innerHTML = document.getElementById(b).value;
-				
-				document.getElementById(three).style.display = "";
-				document.getElementById(three).innerHTML = document.getElementById(c).value;
-				
-				document.getElementById(a).style.display = "none";
-				document.getElementById(b).style.display = "none";
-				document.getElementById(c).style.display = "none";
-			}
+	<head>
+		<title>Edit</title>
+		<script type="text/javascript">
+		function setDate(){
+			var m_names = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+			
+			var now = new Date();
+			var curr_date = now.getDate();
+			var curr_month = now.getMonth();
+			var curr_year = now.getFullYear();
+			document.getElementById("a").value= m_names[curr_month] + " " + curr_date + " " + curr_year;
 		}
-		function textAreaAdjust(o) {
-			o.style.height = "1px";
-			o.style.height = (25+o.scrollHeight)+"px";
-		}
-	</script>
-	<style>
+		</script>
+		<style>
 		body {
 			-webkit-font-smoothing: antialiased;
 			font: 14px/1.8em "PT Serif", serif;
-		}
-		.original {
-			width: 657.586px;
-			padding: 3px;
 		}
 		textarea {
 			padding: 0px;
@@ -125,33 +106,16 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 			font: inherit;
 		}
 	</style>
-</head>
-<body>
-	<article id="0" onclick="edit('1', '2', '3', 'a', 'b', 'c', 'submit1')">
-		<span class="date" id="1"><?php echo "Date & Time"; ?></span>
-		<h1 class="title" id="2" class="original"><?php echo "File Name"; ?></h1>
-		<div id="3" class="original"><?php echo "Content goes here"; ?></div>
-	</article>
+	</head>
 	
-	<form name="newPost" action="create.php" method="post" enctype="multipart/form-data">
-		<textarea id="a" class="editable" name="date" rows="1" style="display:none;resize:none;"></textarea><br />
-		<textarea id="b" class="editable" name="filename" rows="1" style="display:none;resize:none;"></textarea><br />
-		<textarea id="c" class="editable" name="content" style="display:none;"></textarea><br />
-		<input id="submit1" class="editable" type="submit" value="Done" style="display:none;resize:resize;" onclick="edit()"/>
-	</form>
-	
-	<article id="1" onclick="edit('4', '5', '6', 'd', 'e', 'f', 'submit2')">
-		<div id="4" class="original"><?php echo "2Date & Time"; ?></div>
-		<div id="5" class="original"><?php echo "2File Name"; ?></div>
-		<div id="6" class="original"><?php echo "2Content goes here"; ?></div>
-	</article>
-
-	<form name="newPost" action="create.php" method="post" enctype="multipart/form-data">
-		<textarea id="d" class="editable" name="date" rows="1" style="display:none;resize:none;"></textarea><br />
-		<textarea id="e" class="editable" name="filename" rows="1" style="display:none;resize:none;"></textarea><br />
-		<textarea id="f" class="editable" name="content" style="display:none;"></textarea><br />
-		<input id="submit2" class="editable" type="submit" value="Done" style="display:none;resize:resize;" onclick="edit()"/>
-	</form>
-	
-</body>
+	<body onload="setDate()">
+		<form method="POST" name="newPost" action="create.php" enctype="multipart/form-data">
+			<textarea id="a" class="editable" name="date" rows="1" style="resize:none;"></textarea><br />
+			<textarea id="b" class="editable" name="filename" rows="1" style="resize:none;" placeholder="Title"></textarea><br />
+			<textarea id="c" class="editable" name="content" placeholder="Content"></textarea><br />
+			<textarea id="d" class="editable" name="passkey" rows="1" style="resize:none;" placeholder="key"></textarea><br />
+			<textarea id="e" class="editable" name="url" rows="1" style="resize:none;" placeholder="URL"></textarea><br />
+			<input id="submit1" class="editable" type="submit" value="Done" style="resize:resize;" onclick="edit()"/>
+		</form>
+	</body>
 </html>
